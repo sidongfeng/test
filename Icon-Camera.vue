ee9fec5eb041f2fcd3e3c081c7d77f37a6b9f383
Sidong Feng <37397706+sidongfeng@users.noreply.github.com>
2020-09-29 15:57:09 +0800

    Create Icon-Camera.vue

---
+++ Icon-Camera.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="icon">
+      <div class="icon-camera"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Icon-Camera',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
